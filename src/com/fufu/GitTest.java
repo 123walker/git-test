2437f6199d1295c439cdb53b1eddf5af193d9c86
OginoChihiro <OginoChihiro.email>
2023-07-01 20:05:08 +0800

    third commit

--- src/com/fufu/GitTest.java
+++ src/com/fufu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("fufu");
         System.out.println("大傻");
+        System.out.println("樱樱");
     }
 }
